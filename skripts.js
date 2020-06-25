f89df9ff6575fd62409dbb3b42130fae2528ad80
RomanLamer <lamer11.02.27@gmail.com>
2020-06-25 19:20:39 +0300

    v1.0.0

---
+++ skripts.js
@@ -0,0 +1,29 @@
+document.onclick = function (event) {
+    let tar =  event.target;
+    if (tar.classList.value === 'nav_item') {
+            document.querySelectorAll('.nav_item').forEach((e)=>{
+                e.classList.value = 'nav_item' 
+            });
+        tar.classList.add('active');
+        document.querySelectorAll('.list').forEach((e)=>{
+        e.classList.value = 'list none'
+        })
+        document.getElementById(tar.innerText).classList.remove('none');
+        document.getElementById(tar.innerText).classList.add('wisble');
+    }
+}
+const API_KEY = "3f42f0c2fce8618134591d05ef21004a"; 
+const btn  = document.querySelector('button');
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const city = document.querySelector('input').value;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`).then(response => {return response.json()}).then(data => {
+    document.getElementById('name').innerText =  data.name;
+    document.getElementById('country').innerText =  data.sys.country;
+    document.getElementById('deg').innerText =  Math.round(data.main.temp-273)+"°";
+    document.getElementById('deg1').innerText =  Math.round(data.main.temp-273)+"°";
+    document.getElementById('icon').src =  `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`;
+    console.log(data);
+    
+}).then(document.querySelector('.card').classList.remove('none')).catch(alert('asd'));
+})
