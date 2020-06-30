64861d83cb6c527821f8582a78059994e97eab31
RomanLamer <lamer11.02.27@gmail.com>
2020-06-30 15:34:11 +0300

    Adaptiv

--- scripts.js
+++ scripts.js
@@ -17,29 +17,29 @@ const API_KEY = "3f42f0c2fce8618134591d05ef21004a";
 const btn  = document.querySelector('button');
 btn.addEventListener('click',(e)=>{
     e.preventDefault();
-    const city = document.querySelector('input').value;
+    let city = document.querySelector('input').value;
+    if (city != '') {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`).then(response => {return response.json()}).then(data => {
     document.getElementById('name').innerText =  data.name;
     document.getElementById('country').innerText =  data.sys.country;
     document.getElementById('deg1').innerText =  Math.round(data.main.temp-273)+"°";
-    document.getElementById('icon').src =  `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`;
     console.log(data)
+    let cuurentDay = 0;
+    let myTime;
+    if (new Date().getHours()-3 + data.timezone / 3600 >= 0) {
+        myTime =new Date().getHours()-3 + data.timezone / 3600
+    }else{
+        myTime =new Date().getHours()-3 + data.timezone / 3600 +24
+        cuurentDay = -1
+    }
+    let AmPm = "a.m."
+    if (myTime > 12) {
+        myTime = myTime - 12;
+        AmPm = "p.m.";
+    }
+    let time  = `${myTime}:${new Date().getMinutes()<10?"0"+new Date().getMinutes(): new Date().getMinutes()} ${AmPm}`    
+    document.getElementById('time').innerHTML = time;
     setInterval(()=>{
-        let cuurentDay = 0;
-        let myTime;
-        if (new Date().getHours()-3 + data.timezone / 3600 >= 0) {
-            myTime =new Date().getHours()-3 + data.timezone / 3600
-        }else{
-            myTime =new Date().getHours()-3 + data.timezone / 3600 +24
-            cuurentDay = -1
-        }
-        let AmPm = "AM"
-        if (myTime > 12) {
-            myTime = myTime - 12;
-            AmPm = "PM";
-        }
-        let time  = `${myTime}:${new Date().getMinutes()<10?"0"+new Date().getMinutes():new Date().getMinutes()} ${AmPm}`    
-        document.getElementById('time').innerHTML = time;
         let day;
         switch (new Date().getDay() +cuurentDay) {
             case 1:
@@ -123,20 +123,48 @@ btn.addEventListener('click',(e)=>{
             myTime =new Date().getHours()-3 + data.timezone / 3600 +24
             myDay = -1
         }
-        document.getElementById('deg').innerText =  Math.round(content.hourly[myTime].temp - 273)+"°";
-        for (let i = 0; i < 6 ; i++) {
-        document.getElementById(`mydeg${i}`).innerHTML = Math.round(content.hourly[myTime+i+1].temp - 273)+"°";
-        document.getElementById(`currentTime${i}`).innerHTML = myTime+i+1 + ":00";   
-        document.getElementById(`icon${i}`).src =  `https://openweathermap.org/img/wn/${content.hourly[myTime+i+1].weather[0]['icon']}@2x.png`;
+        for (let i = 0; i < 7 ; i++) {
+        let li =  document.createElement('li');li.classList.add('list_item');
+        let mydiv = document.createElement('div');mydiv.classList.add('listDegrees');mydiv.innerHTML = Math.round(content.hourly[myTime+i+1].temp - 273)+"°";
+        let myp = document.createElement('p');myp.classList.add('listTime');myp.innerHTML= i === 0 ?"Now" : myTime+i > 23? myTime+i-24+":00": myTime+i + ":00";
+        let cont = document.createElement('div');cont.classList.add('cont')
+        let myimg = document.createElement('img');myimg.src = `https://openweathermap.org/img/wn/${content.hourly[myTime+i+1].weather[0]['icon']}@2x.png`;
+        li.appendChild(myp)
+        li.appendChild(cont)
+        cont.appendChild(myimg)
+        cont.appendChild(mydiv)
+        document.getElementById('Today').appendChild(li)
         }
         for (let i = 0; i <= 6 ; i++) {
         let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-        document.getElementById(`myDay${i}`).innerHTML = Math.round(content.daily[i].temp.day - 273)+"°";
-        document.getElementById(`currentDay${i}`).innerHTML = days[new Date().getDay() +myDay -1+i];   
-        document.getElementById(`iconDay${i}`).src =  `https://openweathermap.org/img/wn/${content.daily[i].weather[0]['icon']}@2x.png`;
+        let li =  document.createElement('li');li.classList.add('list_item');
+        let mydiv = document.createElement('div');mydiv.classList.add('listDegrees');mydiv.innerHTML =  Math.round(content.daily[i].temp.day - 273)+"°";
+        let myp = document.createElement('p');myp.classList.add('listTime');myp.innerHTML=days[new Date().getDay() +myDay -1+i];
+        let cont = document.createElement('div');cont.classList.add('cont')
+        let myimg = document.createElement('img');myimg.src = `https://openweathermap.org/img/wn/${content.daily[i].weather[0]['icon']}@2x.png`;
+        li.appendChild(myp)
+        li.appendChild(cont)
+        cont.appendChild(myimg)
+        cont.appendChild(mydiv)
+        document.getElementById('Week').appendChild(li)
         }
     })
-    }).then(document.querySelector('.card').classList.remove('none'));
+    }).then(()=>{ if ( document.getElementById('name').innerText != "undefined") {
+    document.querySelector('.card').classList.remove('none')
+    document.querySelector('.back').classList.remove('none')   
+    }else{
+        alert("Invalid city name");
+    }
+});       
+}else{
+    alert("Invalid city name")}
+})
+document.querySelector('.back').addEventListener('click',()=>{
+    document.querySelector('.card').classList.add('none')
+    document.getElementById('Week').innerHTML= "";
+    document.getElementById('Today').innerHTML= "";
+    document.querySelector('input').value = ""
+        
 })
 
 //3f42f0c2fce8618134591d05ef21004a
